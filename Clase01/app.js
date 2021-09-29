396bbdbb8bd2e7db8f1ef3871596d118ce0dc06e
Conrado Lanusse <conrado.lanusse@gmail.com>
2021-09-29 10:55:07 -0300

    Clase 01 - Sintaxis y conceptos generales

---
+++ Clase01/app.js
@@ -0,0 +1,84 @@
+// Clase 01 - Sintaxis general
+
+// declaracion => let / const
+
+// var profesor
+
+const profesor = "Conrado Lanusse"
+
+// profesor = "Conrado Lanusse"
+
+console.log( profesor ) 
+
+let primerNota = 10
+let segundaNota = 20
+
+
+let resultado = primerNota * segundaNota
+
+// alert(resultado)
+
+// console.log( primerNota + segundaNota )
+
+console.log(segundaNota)
+
+segundaNota = 8
+
+console.log(segundaNota)
+
+let promedio = (primerNota + segundaNota) / 2
+
+// alert(promedio)
+
+// ==================
+
+// let nombre = "Martin"
+// let apellido = "Bonino"
+
+// let nombre = prompt('Ingrese su nombre')
+// let apellido = prompt("Ingrese su 'apellido' ")
+
+// let nombreCompleto = nombre + " " + apellido + ": El mejor tutor del multiverso"
+
+// alert( nombreCompleto )
+
+// console.log( nombreCompleto )
+
+// console.log( 10 - "10" )
+
+const interes = 1.30
+
+let monto = parseInt( prompt('Ingrese el monto') )
+
+// let monto = prompt('Ingrese el monto')
+// monto = parseInt( monto )
+// monto = Number( monto )
+
+console.log( "Su monto total a pagar es: " + interes * monto)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
